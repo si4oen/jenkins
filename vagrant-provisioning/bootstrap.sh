8e400b9d5455a3f6ee4d7892e7627b7c20fce0f4
Si4oen <si4oen@users.noreply.github.com>
2020-01-17 14:18:27 +0700

    Update bootstrap.sh

--- vagrant-provisioning/bootstrap.sh
+++ vagrant-provisioning/bootstrap.sh
@@ -46,8 +46,8 @@ EOF
 echo ">>>>> [TASK] Install Jenkins & Git on CentOS 7"
 yum install -y git >/dev/null 2>&1
 yum -y install java-1.8.0-openjdk >/dev/null 2>&1
-curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo >/dev/null 2>&1
-rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo >/dev/null 2>&1
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum -y install jenkins >/dev/null 2>&1
 systemctl daemon-reload
 systemctl start jenkins
